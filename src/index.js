94e20b59ad265c0bb76392da44ec18d8ee7c4187
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-07-30 23:33:25 +0300

    init: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n).split('').reverse();
+    let x = str.length-1;
+    if (str[0] == '0') {
+        str.splice(0, 1);
+    };
+    if (str[x] == '-') {
+       str.splice(x, 1);
+    };
+            let nbr = Number(str.join(''));
+            return nbr
 }
